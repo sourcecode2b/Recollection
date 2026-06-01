c53bee55c5c82ce8e5d9fe15f30f15f39423ee99
sourcecode2b <Luke@DESKTOP-J2RJFED>
2026-05-31 19:36:51 -0500

    title scrreensssasa

--- src/main/java/net/mcreator/recollection/client/screens/Titlescreen3Overlay.java
+++ src/main/java/net/mcreator/recollection/client/screens/Titlescreen3Overlay.java
@@ -5,12 +5,13 @@ import org.checkerframework.checker.units.qual.h;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.Minecraft;
 
 import net.mcreator.recollection.procedures.TitlescreenoverlayshowProcedure;
@@ -18,22 +19,24 @@ import net.mcreator.recollection.procedures.TitlescreenoverlayshowProcedure;
 @Mod.EventBusSubscriber({Dist.CLIENT})
 public class Titlescreen3Overlay {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public static void eventHandler(RenderGuiEvent.Pre event) {
-		int w = event.getWindow().getGuiScaledWidth();
-		int h = event.getWindow().getGuiScaledHeight();
-		Level world = null;
-		double x = 0;
-		double y = 0;
-		double z = 0;
-		Player entity = Minecraft.getInstance().player;
-		if (entity != null) {
-			world = entity.level();
-			x = entity.getX();
-			y = entity.getY();
-			z = entity.getZ();
-		}
-		if (TitlescreenoverlayshowProcedure.execute()) {
-			event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.recollection.titlescreen_3.label_leave_the_game"), 2, 5, -65536, false);
+	public static void eventHandler(ScreenEvent.Render.Post event) {
+		if (event.getScreen() instanceof TitleScreen) {
+			int w = event.getScreen().width;
+			int h = event.getScreen().height;
+			Level world = null;
+			double x = 0;
+			double y = 0;
+			double z = 0;
+			Player entity = Minecraft.getInstance().player;
+			if (entity != null) {
+				world = entity.level();
+				x = entity.getX();
+				y = entity.getY();
+				z = entity.getZ();
+			}
+			if (TitlescreenoverlayshowProcedure.execute()) {
+				event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.recollection.titlescreen_3.label_leave_the_game"), 2, 5, -65536, false);
+			}
 		}
 	}
 }
--- src/main/java/net/mcreator/recollection/procedures/TitlescreenoverlayshowProcedure.java
+++ src/main/java/net/mcreator/recollection/procedures/TitlescreenoverlayshowProcedure.java
@@ -2,7 +2,7 @@ package net.mcreator.recollection.procedures;
 
 public class TitlescreenoverlayshowProcedure {
 	public static boolean execute() {
-		if (Math.random() > 0.9) {
+		if (Math.random() > 0.99) {
 			return true;
 		}
 		return false;
