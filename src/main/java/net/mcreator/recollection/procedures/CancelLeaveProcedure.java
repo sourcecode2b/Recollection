f360477742849733fef78960560aef0476c8ca2e
Totoro672YT <totoro6seven2@gmail.com>
2026-06-01 16:02:52 -0400

    Yes

--- src/main/java/net/mcreator/recollection/procedures/CancelLeaveProcedure.java
+++ src/main/java/net/mcreator/recollection/procedures/CancelLeaveProcedure.java
@@ -1,6 +1,13 @@
 package net.mcreator.recollection.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 public class CancelLeaveProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
