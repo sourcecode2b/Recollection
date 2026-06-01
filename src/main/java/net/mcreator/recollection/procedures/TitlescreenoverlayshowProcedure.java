e8e650a02e438ec5ca431f03d7fd038f8b7c74b9
sourcecode2b <Luke@DESKTOP-J2RJFED>
2026-05-31 19:34:38 -0500

    titlescreen cooked

---
+++ src/main/java/net/mcreator/recollection/procedures/TitlescreenoverlayshowProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.recollection.procedures;
+
+public class TitlescreenoverlayshowProcedure {
+	public static boolean execute() {
+		if (Math.random() > 0.9) {
+			return true;
+		}
+		return false;
+	}
+}
