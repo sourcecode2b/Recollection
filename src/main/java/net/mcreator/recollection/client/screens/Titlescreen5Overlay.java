8b6233007528c4ec543db4efa09d8a23d8ffe034
sourcecode2b <Luke@DESKTOP-J2RJFED>
2026-06-01 17:24:17 -0500

    tweaked

---
+++ src/main/java/net/mcreator/recollection/client/screens/Titlescreen5Overlay.java
@@ -0,0 +1,42 @@
+package net.mcreator.recollection.client.screens;
+
+import org.checkerframework.checker.units.qual.h;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.recollection.procedures.TitlescreenoverlayshowProcedure;
+
+@Mod.EventBusSubscriber({Dist.CLIENT})
+public class Titlescreen5Overlay {
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void eventHandler(ScreenEvent.Render.Post event) {
+		if (event.getScreen() instanceof TitleScreen) {
+			int w = event.getScreen().width;
+			int h = event.getScreen().height;
+			Level world = null;
+			double x = 0;
+			double y = 0;
+			double z = 0;
+			Player entity = Minecraft.getInstance().player;
+			if (entity != null) {
+				world = entity.level();
+				x = entity.getX();
+				y = entity.getY();
+				z = entity.getZ();
+			}
+			if (TitlescreenoverlayshowProcedure.execute()) {
+				event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.recollection.titlescreen_5.label_save_us"), w / 2 + -150, h / 2 + -16, -65536, false);
+			}
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/recollection/client/screens/Titlescreen8Overlay.java
@@ -0,0 +1,42 @@
+package net.mcreator.recollection.client.screens;
+
+import org.checkerframework.checker.units.qual.h;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.recollection.procedures.TitlescreenoverlayshowProcedure;
+
+@Mod.EventBusSubscriber({Dist.CLIENT})
+public class Titlescreen8Overlay {
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void eventHandler(ScreenEvent.Render.Post event) {
+		if (event.getScreen() instanceof TitleScreen) {
+			int w = event.getScreen().width;
+			int h = event.getScreen().height;
+			Level world = null;
+			double x = 0;
+			double y = 0;
+			double z = 0;
+			Player entity = Minecraft.getInstance().player;
+			if (entity != null) {
+				world = entity.level();
+				x = entity.getX();
+				y = entity.getY();
+				z = entity.getZ();
+			}
+			if (TitlescreenoverlayshowProcedure.execute()) {
+				event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.recollection.titlescreen_8.label_leave_the_game"), 185, 211, -65536, false);
+			}
+		}
+	}
+}
